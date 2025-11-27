2a4f1405cbf24f8f4e23f75a78b5414a77cc5b59
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2025-11-27 15:01:35 -0300

    feat: Adds config for the project

---
+++ config/config.py
@@ -0,0 +1,6 @@
+"""This file stores relevant information shared among modules."""
+
+import os
+
+PATH_ROOT: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PATH_DATASET: str = os.path.join(PATH_ROOT, "dataset\\dataset.json")
